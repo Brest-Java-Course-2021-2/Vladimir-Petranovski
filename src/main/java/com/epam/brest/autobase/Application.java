f11e0d202c63c1785429e1c7c563571a84247ecc
avp02 <vpetranovskij@gmail.com>
2021-10-09 12:08:15 +0300

    added maven archetype webapp

---
+++ src/main/java/com/epam/brest/autobase/Application.java
@@ -0,0 +1,7 @@
+package com.epam.brest.autobase;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/epam/brest/autobase/ApplicationTest.java
@@ -0,0 +1,9 @@
+package com.epam.brest.autobase;
+
+import junit.framework.TestCase;
+
+public class ApplicationTest extends TestCase {
+
+    public void testMain() {
+    }
+}
