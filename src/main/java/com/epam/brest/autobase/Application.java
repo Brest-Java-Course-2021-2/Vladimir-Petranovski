ba386a6162762c8b3062b1f1d1195814e4460c19
avp02 <vpetranovskij@gmail.com>
2021-10-16 12:48:50 +0300

    added applicationContext.xml

--- src/main/java/com/epam/brest/autobase/Application.java
+++ src/main/java/com/epam/brest/autobase/Application.java
@@ -1,7 +1,20 @@
 package com.epam.brest.autobase;
 
+import com.epam.brest.autobase.dao.models.Car;
+import com.epam.brest.autobase.dao.models.Driver;
+import com.epam.brest.autobase.dao.models.Request;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Application {
     public static void main(String[] args) {
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml")) {
+            Car car = context.getBean(Car.class);
+            Driver driver = context.getBean(Driver.class);
+            Request request = context.getBean(Request.class);
+            System.out.println(car.getModel());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
