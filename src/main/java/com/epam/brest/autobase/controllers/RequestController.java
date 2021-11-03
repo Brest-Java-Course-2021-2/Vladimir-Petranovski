57d28b6d989f7e2ffcdf60e0722cf66594c4ce5e
avp02 <vpetranovskij@gmail.com>
2021-11-03 10:53:53 +0300

    create page for request, added new-request.html, update-request.html, changed file Autobase.md

--- src/main/java/com/epam/brest/autobase/controllers/RequestController.java
+++ src/main/java/com/epam/brest/autobase/controllers/RequestController.java
@@ -1,11 +1,10 @@
 package com.epam.brest.autobase.controllers;
 
 import com.epam.brest.autobase.interfaces.InitAndDestroyBean;
+import com.epam.brest.autobase.models.Request;
 import com.epam.brest.autobase.services.RequestService;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/requests")
 public class RequestController implements InitAndDestroyBean {
@@ -16,16 +15,45 @@ public class RequestController implements InitAndDestroyBean {
         this.requestService = requestService;
     }
 
-//    @GetMapping()
-//    public String getRequestList(Model model) {
-//            model.addAttribute("requestList", requestService.findAllRequest());
-//            return "requests/request";
+    @GetMapping()
+    public String getRequestList(Model model) {
+            model.addAttribute("requestList", requestService.findAllRequest());
+            return "requests/request";
+    }
+
+    @GetMapping("/new-request")
+    public String showForAddRequest(@ModelAttribute("request") Request request) {
+        return "requests/new-request";
+    }
+
+    @PostMapping()
+    public String addRequest(@ModelAttribute("request") Request request) {
+        requestService.saveRequest(request);
+        return "redirect:/requests";
+    }
+
+    @GetMapping("/{request_id}/update")
+    public String showFormForUpdateRequest(@PathVariable("request_id") Integer request_id, Model model) {
+        model.addAttribute("request", requestService.findRequestById(request_id));
+        return "/requests/update-request";
+    }
+
+    @PostMapping("/{request_id}")
+    public String updateRequest(@ModelAttribute("request") Request request, @PathVariable("request_id") Integer request_id) {
+        requestService.updateRequest(request_id, request);
+        return "redirect:/requests";
+    }
+
+    @GetMapping("/{request_id}/delete")
+    public String deleteRequest(@PathVariable("request_id") Integer request_id) {
+        requestService.deleteRequest(request_id);
+        return "redirect:/requests";
+    }
+
+//    @GetMapping("/{id}")
+//    public String getNameDriverById(@PathVariable("id") Integer id, Model model) {
+//        model.addAttribute("name", requestService.getNameDriverById(id));
+//        return "/requests/request";
 //    }
 
-    @GetMapping()
-    public ModelAndView getRequestList() {
-        ModelAndView modelAndView = new ModelAndView("requests/request");
-        modelAndView.addObject("requestList", requestService.findAllRequest());
-        return modelAndView;
-    }
 }
--- src/main/java/com/epam/brest/autobase/dao/RequestDAO.java
+++ src/main/java/com/epam/brest/autobase/dao/RequestDAO.java
@@ -3,15 +3,12 @@ package com.epam.brest.autobase.dao;
 import com.epam.brest.autobase.interfaces.InitAndDestroyBean;
 import com.epam.brest.autobase.models.Request;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 import static com.epam.brest.autobase.config.JDBCConfiguration.getConnection;
-import static com.epam.brest.autobase.dao.Queries.REQUEST_FIND_ALL;
+import static com.epam.brest.autobase.dao.Queries.*;
 import static com.epam.brest.autobase.logger.Logging.log;
 
 public class RequestDAO implements InitAndDestroyBean {
@@ -35,5 +32,66 @@ public class RequestDAO implements InitAndDestroyBean {
             throwables.printStackTrace();
         }
         return requests;
+    }
+
+    public void saveRequestDAO(Request request) {
+        try(Connection connection = getConnection()) {
+            log.info("Connection established " + getClass().getName());
+            PreparedStatement preparedStatement = connection.prepareStatement(REQUEST_SAVE);
+            preparedStatement.setString(1, request.getName());
+            preparedStatement.setInt(2, request.getDriver_id());
+            preparedStatement.executeUpdate();
+            log.info("Request was save --- {}", request);
+        } catch (Exception e) {
+            log.info("Request wasn't save --- {}", request);
+            e.printStackTrace();
+        }
+    }
+
+    public Request findRequestByIdDAO(Integer request_id) {
+        try(Connection connection = getConnection()) {
+            log.info("Connection established " + getClass().getName());
+            PreparedStatement preparedStatement = connection.prepareStatement(REQUEST_FIND_BY_ID);
+            preparedStatement.setInt(1, request_id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            return Request.builder()
+                    .request_id(resultSet.getInt("request_id"))
+                    .name(resultSet.getString("name"))
+                    .driver_id(resultSet.getInt("driver_id"))
+                    .build();
+        } catch(Exception e) {
+            log.info("Request wasn't find by id --- {}", request_id);
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void updateRequestDAO(Integer request_id, Request requestToUpdate) {
+        try(Connection connection = getConnection()) {
+            log.info("Connection established " + getClass().getName());
+            PreparedStatement preparedStatement = connection.prepareStatement(REQUEST_UPDATE);
+            preparedStatement.setInt(1, request_id);
+            preparedStatement.setString(2, requestToUpdate.getName());
+            preparedStatement.setInt(3, requestToUpdate.getDriver_id());
+            preparedStatement.executeUpdate();
+            log.info("Request {} was update with id {}", requestToUpdate, request_id);
+        } catch (Exception e) {
+            log.info("Request {} wasn't update with id {}", requestToUpdate, request_id);
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteRequestDAO(Integer request_id) {
+        try(Connection connection = getConnection()) {
+            log.info("Connection established " + getClass().getName());
+            PreparedStatement preparedStatement = connection.prepareStatement(REQUEST_DELETE);
+            preparedStatement.setInt(1, request_id);
+            preparedStatement.executeUpdate();
+            log.info("Request was delete with id {}", request_id);
+        } catch (Exception e) {
+            log.info("Request wasn't update with id {}", request_id);
+            e.printStackTrace();
+        }
     }
 }
